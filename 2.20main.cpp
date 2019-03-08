a4d37e9b0bf6f35636997da464045ff981d8b767
linxiaoang <1835584172@qq.com>
2019-03-08 20:24:04 +0800

    测绘18-3孟春雨的作业2.16

---
+++ 2.20main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int r=0;
+    int d=0;
+    int C=0;
+    int S=0;
+    int pi=3.14159;
+    cout<<"Enter the r of circle: ";
+    cin>>r;
+    d=2*r,C=pi*d,S=pi*r*r;
+    cout<<"d is "<<d<<"\nC is "<<C<<"\nS is "<<S<<endl;
+}
