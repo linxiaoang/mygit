aa04ff4e54bf7bef409d10edd472296c502afb22
linxiaoang <1835584172@qq.com>
2019-03-08 20:18:55 +0800

    测绘18-3孟春雨的作业2.17

---
+++ 2.17main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"12 34\n";//a)
+
+    cout<<"1"<<"2 "<<"3"<<"4\n";//b)
+
+    cout<<"1";
+    cout<<"2 ";
+    cout<<"3";
+    cout<<"4";//c)
+
+}
