8c86d4ea55335377ac0025766006f4e515027f84
meng_chunyu <1835584172@qq.com>
2019-05-05 18:23:38 +0800

    第八章作业（内置数组和指针）

---
+++ MCY8.10/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+void zero(long bigIntegers[]);//±àÐ´º¯ÊýÍ·²¿
+int add1AndSum(int oneTooSmall[]);
+int main()
+{
+
+
+
+}
+
+void zero(long bigIntegers[])
+{
+
+}
