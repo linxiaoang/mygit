7a59b3dd627b623fc637e0c0cf0e389bfa201499
meng_chunyu <1835584172@qq.com>
2019-05-12 22:30:45 +0800

    第九章作业

---
+++ MCY9.5/include/Complex.h
@@ -0,0 +1,18 @@
+// Exercise 9.5 Solution: Complex.h
+#ifndef COMPLEX_H
+#define COMPLEX_H
+
+class Complex
+{
+public:
+   Complex( double = 0.0, double = 0.0 );
+   Complex add( const Complex & );
+   Complex subtract( const Complex & );
+   void printComplex();
+   void setComplexNumber( double, double );
+private:
+   double realPart;
+   double imaginaryPart;
+};
+
+#endif
